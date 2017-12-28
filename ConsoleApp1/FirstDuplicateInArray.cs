a376fd686e24c48577f2bc51be4fe1a3b0920c95
hrachG <hrachgulzadyan@gmail.com>
2017-12-28 00:12:30 -0800

    commit

---
+++ ConsoleApp1/FirstDuplicateInArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms
+{
+    public class FirstDuplicateInArray
+    {
+        public static int FirstDuplicate(int[] a)
+        {
+
+            var arr2 = Enumerable.Repeat<int>(0, a.Length + 1).ToArray();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (arr2[a[i]] == 0)
+                {
+                    arr2[a[i]] = a[i];
+                }
+                else
+                {
+                    return a[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 1, 3 };
+            var val = FirstDuplicateInArray.FirstDuplicate(arr);
+            Console.WriteLine(val);
+
+
+        }
+    }
+}
